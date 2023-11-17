62d3ca5b5031c053f2feb7c119d6f39f324900b9
Victor Candido <victor.candido@iv2.com.br>
2023-11-17 13:43:28 -0300

    Section list done

--- app/details.tsx
+++ app/details.tsx
@@ -1,13 +1,19 @@
 import React, { useLayoutEffect } from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, SectionList, ListRenderItem } from 'react-native';
 import ParallaxScrollView from '../Components/ParallaxScrollView';
 import Colors from "../constants/Colors";
 import { restaurant } from "../assets/data/restaurant"; 
-import { useNavigation } from "expo-router";
+import { Link, useNavigation } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
 const Details = () => {
     const navigation = useNavigation();
+
+    const DATA = restaurant.food.map((item, index) => ({
+        title: item.category,
+        data: item.meals,
+        index
+    }));
     
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -33,6 +39,20 @@ const Details = () => {
         });
     }, []);
 
+    const renderItem: ListRenderItem<any> = ({ item, index }) => (
+        <Link href={'/'} asChild>
+            <TouchableOpacity style={styles.item}>
+                <View style={{ flex: 1 }}>
+                    <Text style={styles.dish}>{item.name}</Text>
+                    <Text style={styles.dishText}>{item.info}</Text>
+                    <Text style={styles.dishText}>${item.price}</Text>
+                </View>
+
+                <Image source={item.img} style={styles.dishImage}/>
+            </TouchableOpacity>
+        </Link>
+    )
+
     return (
         <>
         <ParallaxScrollView
@@ -48,7 +68,30 @@ const Details = () => {
             )}
         >
             <View style={styles.detailsContainer}>
-                <Text>Scroll me</Text>
+                <Text style={styles.restaurantName}>{restaurant.name}</Text>
+                
+                <Text style={styles.restaurantDescription}>
+                    { restaurant.delivery } - { restaurant.tags.join(' - ') }
+                </Text>
+
+                <Text style={styles.restaurantDescription}>{restaurant.about}</Text>
+
+                <SectionList
+                    contentContainerStyle={{ paddingBottom: 50 }}
+                    scrollEnabled={false}
+                    keyExtractor={(item, index) => `${item.id + index}`}
+                    sections={DATA}
+                    renderItem={renderItem}
+                    ItemSeparatorComponent={() => (
+                        <View style={styles.separatorComponent}></View>
+                    )}
+                    SectionSeparatorComponent={() => (
+                        <View style={styles.separatorComponent}></View>
+                    )}
+                    renderSectionHeader={({ section: { title, index}}) => (
+                        <Text style={styles.sectionHeader}>{title}</Text>
+                    )}
+                />
             </View>
         </ParallaxScrollView>
         </>
@@ -83,6 +126,47 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         gap: 10,
+    },
+    restaurantName: {
+        fontSize: 30,
+        margin: 16,
+
+    },
+    restaurantDescription: {
+        fontSize: 16,
+        margin: 16,
+        lineHeight: 22,
+        color: Colors.medium,
+    },
+    sectionHeader: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginTop: 40,
+        margin: 16,
+    },
+    separatorComponent: { 
+        height: 1, 
+        backgroundColor: Colors.grey,
+        marginHorizontal: 16,
+    },
+    item: {
+        backgroundColor: 'white',
+        padding: 16,
+        flexDirection: 'row',
+    },
+    dish: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    dishText: {
+        fontSize: 14,
+        color: Colors.mediumDark,
+        paddingVertical: 4,
+    },
+    dishImage: {
+        height: 80,
+        width: 80,
+        borderRadius: 4,
     },
 });
 
